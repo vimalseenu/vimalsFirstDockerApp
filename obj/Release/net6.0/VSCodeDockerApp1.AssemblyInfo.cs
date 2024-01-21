924663847b1fe9795ddea5a5d58472d2e25b78be
Vimal Boomarthi <azvimal@hotmail.com>
2024-01-21 18:14:27 +0530

    "Updated with SystemEnvMachineName"

--- obj/Release/net6.0/VSCodeDockerApp1.AssemblyInfo.cs
+++ obj/Release/net6.0/VSCodeDockerApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VSCodeDockerApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3fa813a73a04ae3a0e16b74168fab31bc39af93")]
 [assembly: System.Reflection.AssemblyProductAttribute("VSCodeDockerApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VSCodeDockerApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
